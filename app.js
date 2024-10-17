0b19bdd890ac29024fae8db90b0773b451820769
Nikhil <nikhilgod0114@gmail.com>
2024-10-17 17:41:47 +0530

    add- winnerMsg , enable-disable button functions, reset btn , hide class in the msg-container

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 let boxes = document.querySelectorAll(".box");
-let resetbtn = document.querySelectorAll(".reset");
+let resetbtn = document.querySelector(".reset");
+let NewGameBTN = document.querySelector("#new-btn");
+let msg = document.querySelector("#msg");
+let msgContainer = document.querySelector(".msg-container");
+
  let turn = true;
 const WinPattern = [
     [0,1,2],
@@ -11,6 +15,12 @@ const WinPattern = [
     [0,4,8],
     [2,4,6]
 ];
+
+const resetfun = ()=>{
+       turn  = true;
+       enable();
+       msgContainer.classList.add("hide");
+}
 
 boxes.forEach((box) =>{
     box.addEventListener("click" , ()=>{
@@ -26,7 +36,23 @@ boxes.forEach((box) =>{
       checkwinner();
     }); 
 })
-
+ 
+ const disable = ()=>{
+     for(let box of boxes){
+        box.disabled = true;
+     }
+ }
+ const enable = ()=>{
+     for(let box of boxes){
+        box.disabled = false;
+        box.innerText ="";
+     }
+    }
+ const showWinner = (winner)=>{
+     msg.innerHTML = `congratulation winner is ${winner}`;
+     msgContainer.classList.remove("hide");
+     disable();
+ }
 const checkwinner = ()=>{
     for(pattern of WinPattern){
       let pos1val = boxes[pattern[0]].innerText;
@@ -36,7 +62,10 @@ const checkwinner = ()=>{
       if(pos1val!=""  && pos2val!="" && pos3val!=""){
         if(pos1val === pos2val && pos2val ===pos3val){
             console.log("winner is " + pos1val);
+           showWinner(pos1val);
         }
       }
  }
 }
+
+resetbtn.addEventListener("click" , resetfun);
